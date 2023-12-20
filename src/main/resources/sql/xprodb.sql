5161c28f7ecad9d281969d199a66302a5b7dc865
곽기진 <upper0house@gmail.com>
2023-12-20 09:08:24 +0900

    fix: 필요없는 DB 주석 삭제

--- src/main/resources/sql/xprodb.sql
+++ src/main/resources/sql/xprodb.sql
@@ -50,57 +50,6 @@ CREATE TABLE IF NOT EXISTS TDG_INFO (
                                         ACCT_DAY_CLOSE CHAR,
                                         PRT_APT_YN CHAR
 );
-comment on table TDG_INFO is '단지정보';
-comment on column TDG_INFO.APT_CD is '아파트코드';
-comment on column TDG_INFO.APT_NM is '아파트명';
-comment on column TDG_INFO.APT_OUTPUT_NM is '아파트명_출력명';
-comment on column TDG_INFO.APT_FORM is '아파트형태구분';
-comment on column TDG_INFO.APT_NO_NUM is '총동수';
-comment on column TDG_INFO.HOUSEHOLD_NUM is '총세대수';
-comment on column TDG_INFO.LEASE_CLASS is '임대구분';
-comment on column TDG_INFO.APT_STATE is '상태코드';
-comment on column TDG_INFO.FIRST_CONT_DATE is '최초계약일자';
-comment on column TDG_INFO.HAPSAN_YN is '합산동호여부';
-comment on column TDG_INFO.MEMO is '메모';
-comment on column TDG_INFO.REG_DATE is '등록일자';
-comment on column TDG_INFO.REG_USER_ID is '등록자';
-comment on column TDG_INFO.UPD_DATE is '수정일자';
-comment on column TDG_INFO.UPD_USER_ID is '수정자';
-comment on column TDG_INFO.COSM_CD is '입주자관리번호';
-comment on column TDG_INFO.NOTI_KIND is '고지서종류';
-comment on column TDG_INFO.ADJU_KIND is '조정대장종류';
-comment on column TDG_INFO.ETRT_COM is '위탁사';
-comment on column TDG_INFO.IT_PROC_COM is '전산업체';
-comment on column TDG_INFO.CONT_START_DT is '계약시작일자';
-comment on column TDG_INFO.CONT_END_DT is '계약종료일자';
-comment on column TDG_INFO.REQ_DT is '요청일자';
-comment on column TDG_INFO.APT_NO_FR is '동 시작';
-comment on column TDG_INFO.APT_NO_TO is '동 끝';
-comment on column TDG_INFO.APT_ROOM_FR is '호 시작';
-comment on column TDG_INFO.APT_ROOM_TO is '호 끝';
-comment on column TDG_INFO.OFFICE_MNG_NO is '관리사무소';
-comment on column TDG_INFO.ETC_NOTI_KIND is '별도고지서 종류';
-comment on column TDG_INFO.DBID is 'SYSTEM-ID';
-comment on column TDG_INFO.APT_TYPE is '단지종류';
-comment on column TDG_INFO.CONV_YYMM is '컨버젼년월(수납조건월)';
-comment on column TDG_INFO.CONV_ETC1_YYMM is '별도1컨버젼년월(수납조건월)';
-comment on column TDG_INFO.OUTPUT_REQ is '자체출력 여부';
-comment on column TDG_INFO.TEAM_CODE is '관리팀 코드';
-comment on column TDG_INFO.PA_TA_CLOSE is '연말정산마감정보';
-comment on column TDG_INFO.SDIV is '출력자동화';
-comment on column TDG_INFO.SMILE_EDI is '전자세금계산서종류(B빌메이트Y스마일EBMERP)';
-comment on column TDG_INFO.RCPT_CREAT is '당월미납생성';
-comment on column TDG_INFO.RCPT_DIV is '분리부과기본값(1부과2비율3연체';
-comment on column TDG_INFO.EXPORT_CLASS is '엑셀변환(입검부수회인민위기)';
-comment on column TDG_INFO.REPORT_TYPE is '부과내역서종류';
-comment on column TDG_INFO.REPORT_PAGE is '부과내역서부수';
-comment on column TDG_INFO.GRP_APT_CD is '아파트그룹';
-comment on column TDG_INFO.HANGEUL_YN is '한글동호';
-comment on column TDG_INFO.ACCT_STD_CD is '회계표준';
-comment on column TDG_INFO.APTI_USR_ID is '아파트아이아이디';
-comment on column TDG_INFO.APTI_PW is '아파트아이비밀번호';
-comment on column TDG_INFO.ACCT_DAY_CLOSE is '회계일마감';
-comment on column TDG_INFO.PRT_APT_YN is '출력동호여부';
 
 -- XPRODB 에만 넣는다
 INSERT INTO TDG_INFO (APT_CD, APT_NM, APT_OUTPUT_NM, APT_FORM, APT_NO_NUM, HOUSEHOLD_NUM, LEASE_CLASS, APT_STATE, FIRST_CONT_DATE, HAPSAN_YN, MEMO, REG_DATE, REG_USER_ID, UPD_DATE, UPD_USER_ID, COSM_CD, NOTI_KIND, ADJU_KIND, ETRT_COM, IT_PROC_COM, CONT_START_DT, CONT_END_DT, REQ_DT, APT_NO_FR, APT_NO_TO, APT_ROOM_FR, APT_ROOM_TO, OFFICE_MNG_NO, ETC_NOTI_KIND, DBID, APT_TYPE, CONV_YYMM, CONV_ETC1_YYMM, OUTPUT_REQ, TEAM_CODE, PA_TA_CLOSE, SDIV, SMILE_EDI, RCPT_CREAT, RCPT_DIV, EXPORT_CLASS, REPORT_TYPE, REPORT_PAGE, GRP_APT_CD, HANGEUL_YN, ACCT_STD_CD, APTI_USR_ID, APTI_PW, ACCT_DAY_CLOSE, PRT_APT_YN) VALUES ('99999', '테스트', null, '7', 4, 322, null, '1', null, null, null, TIMESTAMP '2013-11-14 16:59:02', 'jubyung2', TIMESTAMP '2020-03-17 11:51:31', 'rayn25', null, '0125', '1101', '95001', '95001', null, null, null, '0001', '9999', 'F001000100', 'F001000100', null, '0215', 'HJDB', null, '199901', null, 'N', null, '2017', 'N|T', 'B', 'Y', '1', 'NYYNNYYYY', 'N', null, null, '1', '0', null, null, 'N', null);
